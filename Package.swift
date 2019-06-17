cbc551899d8d00889620a56c09a926fd0f598508
Neil Tiffin <neil@fairindsoft.com>
2019-06-17 17:22:25 -0500

    'dependencies' must precede argument 'exclude'

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XCGLogger",
-            exclude: ["../ObjcExceptionBridging"],
-            dependencies: []),
+            dependencies: [],
+            exclude: ["../ObjcExceptionBridging"]),
         .testTarget(
             name: "XCGLoggerTests",
             dependencies: ["XCGLogger"]),
