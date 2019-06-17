60b8d9c6af2b245a21d4f31b1f4b45e99b67592a
Neil Tiffin <neil@fairindsoft.com>
2019-06-17 17:19:50 -0500

    Move exceptions to target.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ import PackageDescription
 
 let package = Package(
     name: "XCGLogger",
-    exclude: ["Sources/ObjcExceptionBridging"],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -24,6 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XCGLogger",
+            exclude: ["../ObjcExceptionBridging"],
             dependencies: []),
         .testTarget(
             name: "XCGLoggerTests",
