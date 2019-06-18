1502e159a3e0a02ff0ba7816787368abbfecedba
Neil Tiffin <neil@fairindsoft.com>
2019-06-17 19:23:40 -0500

    Comment out non linux, add exclusion to tests.

--- Package.swift
+++ Package.swift
@@ -24,43 +24,44 @@ let package = Package(
         .target(
             name: "XCGLogger",
             dependencies: [],
-            exclude: ["../ObjcExceptionBridging"]),
+            exclude: ["../ObjcExceptionBridging", "ObjcExceptionBridging"]),
         .testTarget(
             name: "XCGLoggerTests",
             dependencies: ["XCGLogger"]),
+            exclude: ["../ObjcExceptionBridging", "ObjcExceptionBridging"]),
     ]
 )
 
-#else
-
-let package = Package(
-   name: "XCGLogger",
-   products: [
-       // Products define the executables and libraries produced by a package, and make them visible to other packages.
-       .library(
-           name: "XCGLogger",
-           targets: ["XCGLogger"]),
-       .library(
-           name: "ObjcExceptionBridging",
-           targets: ["ObjcExceptionBridging"]),
-   ],
-   dependencies: [
-       // Dependencies declare other packages that this package depends on.
-       // .package(url: /* package url */, from: "1.0.0"),
-   ],
-   targets: [
-       // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-       // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-       .target(
-           name: "XCGLogger",
-           dependencies: ["ObjcExceptionBridging"]),
-       .testTarget(
-           name: "XCGLoggerTests",
-           dependencies: ["XCGLogger"]),
-       .target(
-           name: "ObjcExceptionBridging",
-           dependencies: []),
-   ]
-)
+//#else
+//
+//let package = Package(
+//   name: "XCGLogger",
+//   products: [
+//       // Products define the executables and libraries produced by a package, and make them visible to other packages.
+//       .library(
+//           name: "XCGLogger",
+//           targets: ["XCGLogger"]),
+//       .library(
+//           name: "ObjcExceptionBridging",
+//           targets: ["ObjcExceptionBridging"]),
+//   ],
+//   dependencies: [
+//       // Dependencies declare other packages that this package depends on.
+//       // .package(url: /* package url */, from: "1.0.0"),
+//   ],
+//   targets: [
+//       // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+//       // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+//       .target(
+//           name: "XCGLogger",
+//           dependencies: ["ObjcExceptionBridging"]),
+//       .testTarget(
+//           name: "XCGLoggerTests",
+//           dependencies: ["XCGLogger"]),
+//       .target(
+//           name: "ObjcExceptionBridging",
+//           dependencies: []),
+//   ]
+//)
 
 #endif
